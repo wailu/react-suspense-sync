23347d46df0861e605a7b2fe9558522800639f6e
Wai Lun Lim <42461097+wailu@users.noreply.github.com>
2023-12-09 05:07:50 +0800

    Update example

--- example/client/components/Duck.jsx
+++ example/client/components/Duck.jsx
@@ -16,9 +16,9 @@ const Duck = () => {
     <div style={{ border: "1px solid black", margin: "4px 0", padding: 4 }}>
       <div>This is the duck component</div>
       <p style={{ backgroundColor: count % 2 !== 0 ? "gold" : "initial" }}>
-        If I&apos;m blinking it means I&apos;ve been hydrated! (I should be
+        If I&apos;m blinking it means I&apos;ve been hydrated! I should be
         blinking before the cat component is loaded due to{" "}
-        <b>selective hydration</b>)
+        <b>selective hydration</b>.
       </p>
     </div>
   );
